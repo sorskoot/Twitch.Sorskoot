b824b05e88d0369eec1b1c75a6924248ed8256e7
Timmy Kokke <mental_c@hotmail.com>
2019-04-23 23:51:17 +0200

    finished end stream credits.

--- routes/endstream-credits.js
+++ routes/endstream-credits.js
@@ -1,29 +1,58 @@
 const fs = require('fs');
+const request = require('request');
+
 require('dotenv').config();
 
 const express = require('express');
 const router = express.Router();
-const followersFile = 'recent_followers.txt'; //'session_followers.txt';
+const followersFile = 'session_followers.txt'; //'recent_followers.txt';//
+const subscriberFile = 'session_subscribers.txt';
+const donatorsFile = 'session_donators.txt';
+const hostsFile = 'session_hosts.txt';
+
 const folder = process.env.MUXY_FOLDER;
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   let credits = {};
 
-  credits.followers = await readFollowers();
+  credits.followers = await getUserInfo(followersFile);
+  credits.subscribers = await getUserInfo(subscriberFile);
+  credits.hosts = await getUserInfo(hostsFile);
+  credits.donators = await getUserInfo(donatorsFile);
+
+  if (!credits.followers.length &&
+    !credits.subscribers.length &&
+    !credits.hosts.length &&
+    !credits.donators.length) {
+    credits.nothing = true;
+  }
 
   res.render('endstream-credits', credits);
 });
 
-function readFollowers() {
+async function getUserInfo(file) {
+  const users = await readFile(file);
+  if (users.length) {
+    const fullUsers = await getUsers(users);
+    return fullUsers.map(u => {
+      return {
+        display_name: u.display_name,
+        profile_image_url: u.profile_image_url
+      };
+    });
+  } else return [];
+}
+
+function readFile(file) {
   return new Promise((res, rej) => {
-    fs.readFile(`${folder}\\${followersFile}`, 'utf8', (err, followers) => {
+    fs.readFile(`${folder}\\${file}`, 'utf8', (err, names) => {
       if (!!err) {
         console.log(err);
         rej(err);
       }
-      if (followers !== "") {
-        res(followers.split(',').map(v => v.trim()));
+      if (names !== "") {
+        res(names.split(',').map(v => v.trim()));
       } else {
         res([]);
       }
@@ -33,22 +62,22 @@ function readFollowers() {
 
 module.exports = router;
 
-// const fs = require('fs');
-
-// const file = 'session_most_recent_follower.txt';
-// const folder = process.env.MUXY_FOLDER;
-
-// module.exports =
-//     (twitchClient, target) => {
-//         fs.watchFile(`${folder}\\${file}`, () => {
-//             fs.readFile(`${folder}\\${file}`, 'utf8', (err, follower)=> {
-//                 if(!!err){
-//                     console.log(err);
-//                     return;
-//                 }
-//                 if(follower!==""){
-//                     twitchClient.say(target, `Hi @${follower}! Welcome to the coder-sphere.`);
-//                 }
-//             })           
-//         })            
-//     }
+function getUsers(users) {
+  var querystring = users.map(u => `login=${u}`).join('&');
+  //GET https://api.twitch.tv/helix/users?id=<user ID>&id=<user ID>
+  return new Promise((res, rej) => {
+    request.get(`https://api.twitch.tv/helix/users?${querystring}`, {
+      headers: {
+        'client-id': process.env.TWITCH_CLIENTID
+      }
+    }, function (error, response, body) {
+      if (!!error) {
+        rej(error);
+      }
+      if (response.statusCode != 200) {
+        rej(`statuscode: ${response.statusCode}`);
+      }
+      res(JSON.parse(body).data);
+    });
+  });
+}
