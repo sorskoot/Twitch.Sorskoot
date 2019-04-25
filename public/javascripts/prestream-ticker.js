8092b647e3d8ee454dc3f5c492a477c8b9a85eca
Timmy Kokke <mental_c@hotmail.com>
2019-04-25 22:04:25 +0200

    added texts to prestream ticker

--- public/javascripts/prestream-ticker.js
+++ public/javascripts/prestream-ticker.js
@@ -1,16 +1,30 @@
-(function(){
+(function () {
     let messages = [
-        "Almost ready to go live",
-        "Getting ready to write some code",
-        "Grabbing some coffee"
+        "Almost ready to go live...",
+        "Getting ready to write some code...",
+        "Grabbing some coffee...",
+        "Sorting my bubbles...",
+        "We don't code bugs; only random features.",
+        "Hey! It compiles! Ship it!",
+        "Type !command for a list of commands.",
+        "!merch - 15% off when using GLOB15",
+        "We also have a !discord...",
+        "Follow me on !twitter...",
+        "// do not remove this comment.",
+        "I � Unicode!"        
     ];
 
     let ticker = document.querySelector('.ticker-text');
-    let currentMessage = 0;
+    let lastindex = -1;
 
     setInterval(() => {
-        ticker.innerText = messages[currentMessage];
-        currentMessage = (currentMessage+1) % messages.length;
-    }, 5000);   
+        let index;
+        do {
+            index = ~~(Math.random() * messages.length);
+        } while (index === lastindex);
+
+        ticker.innerText = messages[index];
+        lastindex = index;
+    }, 5000);
 
 })();
