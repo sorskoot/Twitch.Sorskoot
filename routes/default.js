4c0b4f18c745b78f1060bffb45061a1bb483fb87
Timmy Kokke <mental_c@hotmail.com>
2019-07-12 15:24:08 +0200

    css glasses. WIP.

--- routes/default.js
+++ routes/default.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/alert', function(req, res, next) {
-  res.render('alert');
+router.get('/alert', function (req, res, next) {
+    res.render('alert');
 });
 
-router.get('/emotes', function(req, res, next) {
-  res.render('emotes');
+router.get('/emotes', function (req, res, next) {
+    res.render('emotes');
 });
-router.get('/speech', function(req, res, next) {
+
+router.get('/speech', function (req, res, next) {
     res.render('speech');
-  });
+});
+
+router.get('/cool', (req, res) => res.render('cool'));
+
 module.exports = router;
